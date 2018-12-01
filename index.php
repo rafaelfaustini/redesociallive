1e5f0f2aa9eb05031dc4fbbe7fd067b45a125ba3
Rafael Faustini Costa de Oliveira <contato@rafaelfaustini.com.br>
2018-12-01 15:07:12 -0200

    Base registro

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+
+echo 'post';
+
+
+
+}
+
+
+ ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title></title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Rede Social</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+      </li>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container">
+
+<form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST" id="registro">
+<div class="form-group mt-5">
+<label>Nome</label>
+<input class="form-control input-lg">
+</div>
+<div class="form-group mt-2">
+<label>Email</label>
+<input class="form-control input-lg">
+</div>
+<div class="form-group mt-2">
+<label>Senha</label>
+<input class="form-control input-lg">
+</div>
+
+<div onclick="eventoclique();" class="btn bg-primary btn-lg text-white">Registrar</div>
+<script>
+
+function eventoclique(){
+  document.getElementById("registro").submit();
+}
+
+
+</script>
+
+</form>
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
